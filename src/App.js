1e8bd5e82397b3a1ff32c8bde2c44fef572d6d04
AnkitaDhimanContriverz <ankita.dhiman@contriverz.com>
2023-06-29 17:18:44 +0530

    29-06-2023

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [val, setVal] = useState("");
+  const [value, setValue] = useState({
+    name: "Ankita",
+    age: 23,
+    post: "React Developer",
+  });
+
+  const update = () => {
+    setValue((prev) => {
+      return { ...prev, age: 24 };
+    });
+  };
+
+  useEffect(() => {
+    console.log("UseEffect in action");
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Select value-</h1>
+      <button onClick={() => setVal(1)}>value 1</button>
+      <button onClick={() => setVal(2)}>value 2</button>
+      <button onClick={() => setVal(3)}>value 3</button>
+      <button onClick={() => setVal(4)}> value 4</button>
+      <p>selected value - {val}</p>
+
+      <h1>My {value.name}</h1>
+      <p>
+        I am {value.name} and age is {value.age} post is {value.post}.
+      </p>
+      <button type="button" onClick={update}>
+        update age
+      </button>
     </div>
   );
 }
